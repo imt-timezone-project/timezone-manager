5c142c0abd9441037ff40481ab9221f6fee55c67
Thomas <thomas.rokicki@telecom-bretagne.eu>
2017-02-01 10:00:53 +0100

    Updates

--- app.js
+++ app.js
@@ -7796,7 +7796,7 @@ var _elm_lang$html$Html$menuitem = _elm_lang$html$Html$node('menuitem');
 var _elm_lang$html$Html$menu = _elm_lang$html$Html$node('menu');
 
 var _user$project$Main$main = _elm_lang$virtual_dom$Native_VirtualDom.staticProgram(
-	_elm_lang$html$Html$text('Hello world!'));
+	_elm_lang$html$Html$text('Hello IMT!'));
 
 var Elm = {};
 Elm['Main'] = Elm['Main'] || {};
