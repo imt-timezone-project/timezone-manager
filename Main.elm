382a88854cd2ee77fef96dcbd69153894e488465
Thomas <thomas.rokicki@telecom-bretagne.eu>
2017-03-14 21:32:22 +0100

    adding forms to retrieve personnal infos

--- Main.elm
+++ Main.elm
@@ -1,7 +1,57 @@
-module Main exposing (..)
-
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 
 main =
-    Html.text "Hello IMT!"
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+
+-- MODEL
+
+type alias Model =
+  { name : String
+  , city : String
+  , country : String
+  }
+
+
+model : Model
+model =
+  Model "" "" ""
+
+
+-- UPDATE
+
+type Msg
+    = Name String
+    | City String
+    | Country String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Name name ->
+      { model | name = name }
+
+    City city ->
+      { model | city = city }
+
+    Country country ->
+      { model | country = country }
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [
+      div [ ] [ text "Welcome to Timezone Manager !" ]
+    , input [ type_ "text", placeholder "Name", onInput Name ] []
+    , input [ type_ "text", placeholder "City", onInput City ] []
+    , input [ type_ "text", placeholder "Country", onInput Country ] []
+    ]
+
+
